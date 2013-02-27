8d3ffec18a691e239979b4fc190ec14fdfb6b4a8
Fizer Khan <fizerkhan@gmail.com>
2013-02-27 16:39:36 +0530

    Added new file.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,26 +1,10 @@
 require([
-		'jquery',
-		'lodash',
-		'backbone',
+        'jquery',
+        '../../shared/helper.js'
+    ],
 
-		// Application.
-		'app',
-
-		// Load Routers.
-		// 'routers/router'
-	],
-
-	function ($, _, Backbone, app) {
-		'use strict';
-
-		// Use jquery's document ready function to start
-		// the app as soon as the DOM was fully loaded.
-		$(function () {
-			app.start({
-				root: window.location.pathname,
-				bootstrap: window.bootstrap || [],
-				tracker: window._gaq || []
-			});
-		});
-	}
+    function ($, helper) {
+        'use strict';
+        console.log('Loaded successfully.');
+    }
 );
